aedfb6757f774a8f4fa19d60aafc33aa3da04181
deslum <randomazer@MacBook-Pro-Randomazer.local>
2016-09-05 22:20:52 +0300

    #160905 fix setup.py

--- setup.py
+++ setup.py
@@ -9,22 +9,22 @@ except ImportError:
     have_cython = False
 
 if have_cython:
-    ext_modules = [Extension("ssdbpy.ssdbpy", ["ssdbpy/ssdbpy.pyx"]),
+    ext_modules = [Extension("cssdbpy.cssdbpy", ["cssdbpy/cssdbpy.pyx"]),
                    ]
     cmdclass = {'build_ext': build_ext}
 else:
     cmdclass = {}
-    ext_modules = [Extension("ssdbpy.ssdbpy", ["ssdbpy/ssdbpy.c"]),
+    ext_modules = [Extension("cssdbpy.cssdbpy", ["cssdbpy/cssdbpy.c"]),
                    ]
 setup(
-    name='ssdbpy',
-    version='0.0.1a',
-    packages=['ssdbpy'],
+    name='cssdbpy',
+    version='0.0.1',
+    packages=['cssdbpy'],
     ext_modules=ext_modules,
     cmdclass=cmdclass,
     author='deslum',
     author_email='randomazer@gmail.com',
-    url='https://github.com//ssdbpy',
+    url='https://github.com/cssdbpy',
     description='High performance SSDB client implemented with Cython',
     long_description=open('README.rst').read(),
 )
